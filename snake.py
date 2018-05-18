32dc15b2263ae7c896cd578cf9b9532f3bb334d2
KoloMenek <38701488+KoloMenek@users.noreply.github.com>
2018-05-18 23:29:22 +0200

    Update snake.py

--- snake.py
+++ snake.py
@@ -30,9 +30,17 @@ corpsImg = pygame.image.load('body.jpg')
 snake_width = 40 
 points_width = 10
 best_score = 0
+highest_score1 = open("record_book.txt","r")
+var = highest_score1.read()
+highest_score1.close()
+highest_score = int(var) if var.isdigit() else 0
 
 
 
+def record_book(higher_score):
+    font = pygame.font.SysFont (None, 40)
+    text = font.render("Record du jeu: "+str(higher_score), True, Green_light)
+    gameDisplay.blit(text,(1025,0))
 
 
 def affiche_best_score(best):
@@ -51,12 +59,20 @@ def points(posx,posy): ###Fonction qui créer les points
 
 
 def crash(score): ###Fonction qui stop le jeu quand on "crash" en écrivant le "vous avez perdu"
+    global highest_score
+    global best_score
     message_display("Vous avez perdu! Votre score: "+str(score))
     pygame.display.update()
     pygame.mixer.Sound.play(death_sound)
     pygame.mixer.music.pause()
     time.sleep(2.5)
     pygame.mixer.music.unpause()
+
+    if highest_score < best_score:
+        highest_score = best_score
+        highest_score2 = open("record_book.txt","w")
+        highest_score2.write(str(highest_score))
+        highest_score2.close()
     game_start_screen()
 
 
@@ -91,6 +107,7 @@ def game_start_screen(): ###Fonction qui fait le Menu quand on allume le jeu
     pressed = False
     gameDisplay.blit(background, (0,0))
     affiche_best_score(best_score)
+    record_book(highest_score)
     while intro:
         event = pygame.event.poll()
         if event.type == pygame.NOEVENT:
@@ -151,25 +168,29 @@ def game_loop(): ###Fonction qui est la base et la logique du jeu
                     y_change = 0
                     x_change = -40
                     direction = 'left'
+                    break
                 elif event.key == pygame.K_RIGHT and direction != 'left':
                     y_change = 0
                     x_change = 40
                     direction = 'right'
+                    break
                 elif event.key == pygame.K_UP and direction !='down':
                     x_change = 0
                     y_change = -40
                     direction = 'up'
+                    break
                 elif event.key == pygame.K_DOWN and direction !='up':
                     x_change = 0
                     y_change = 40
                     direction ='down'
+                    break
             
                 
             ###########################
-        ##
+        
         x += x_change
         y += y_change
-        ##         
+                 
         gameDisplay.fill(green)
 
         snakeHead = []
@@ -185,11 +206,13 @@ def game_loop(): ###Fonction qui est la base et la logique du jeu
 
         snake(snakeLength,List)
         affiche_best_score(best_score)
+        record_book(highest_score)
         scoreboard(score)
         points(posx_start,posy_start)
         
         if best_score < score:
             best_score = score
+
 
         if x > display_width - snake_width or x < 0 or y > display_height - snake_width or y < 0:
             crash(score)
